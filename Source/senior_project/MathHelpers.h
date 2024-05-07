c797ecbfe97724777af3ade11a58966be33868f1
elliptical0 <90520270+elisemaino@users.noreply.github.com>
2024-05-07 16:40:38 -0400

    revised InteractionComponent to expose an Interact event

---
+++ Source/senior_project/MathHelpers.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+
+
+/**
+ * 
+ */
+class SENIOR_PROJECT_API MathHelpers
+{
+public:
+	MathHelpers();
+	~MathHelpers();
+};
